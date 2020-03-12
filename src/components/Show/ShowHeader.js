5cedfe7c1a7ded9ec088cde1b94fef007d7d1e27
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2020-03-12 14:40:46 +0100

    Completed mobile styling for showPage header

--- src/components/Show/ShowHeader.js
+++ src/components/Show/ShowHeader.js
@@ -2,26 +2,28 @@ import React from "react";
 import { useSelector } from "react-redux";
 import placeholder from "../../utils/placeholder.jpg";
 
+import { StyledShowHeader, Rating, Title, Summary } from "../../styling/Show/ShowHeader";
+
 const ShowHeader = () => {
     const { showInfo } = useSelector(state => state.reducer)
 
     // If no showInfo is present (as on first render) return placeholder message
     if (!showInfo.id) {
         return (
-            <div>
+            <StyledShowHeader>
                 <img
                     src={placeholder}
                     alt="Show Poster/Cover"
                 />
-                <p>XX/10</p>
-                <p>This is the title of the TV show, which is very long isn't it?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-            </div>
+                <Rating>XX/10</Rating>
+                <Title>This is the title of the TV show, which is very long isn't it?</Title>
+                <Summary>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </Summary>
+            </StyledShowHeader>
         )
     }
 
     return (
-        <div>
+        <StyledShowHeader>
             <img
                 src={showInfo.image !== null && showInfo.image.medium ?
                     showInfo.image.medium :
@@ -30,17 +32,17 @@ const ShowHeader = () => {
                 alt="Show Poster/Cover"
             />
             {showInfo.rating && showInfo.rating.average !== null ?
-                <p>{showInfo.rating.average}/10</p> :
-                <p>No rating!</p>
+                <Rating className="rating">Rating: {showInfo.rating.average}/10</Rating> :
+                <Rating className="rating">No rating!</Rating>
             }
-            <p>{showInfo.name}</p>
+            <Title>{showInfo.name}</Title>
 
             {/* 
                 NOTE: Summary seems to have embedded HTML tags as part of the summary, coming directly from API
                 TO-DO: Create utility function to strip any element in between <>
             */}
-            <p>{showInfo.summary}</p>
-        </div>
+            <Summary>{showInfo.summary}</Summary>
+        </StyledShowHeader>
     )
 }
 
--- src/styling/Show/ShowHeader.js
+++ src/styling/Show/ShowHeader.js
@@ -1,5 +1,37 @@
 import styled from "styled-components";
 
 export const StyledShowHeader = styled.div`
-   
+    background: #eee;
+    margin-bottom: -10px;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    img {
+        border-radius: 12px;
+        max-width: 80%;
+        width: auto;
+        height: auto;
+        max-height: 400px;
+        align-self: center;
+    }
+    
+    
+`;
+
+export const Rating = styled.p`
+    margin: 5px 0 0;
+    font-weight: 700;
+    align-self: center;
+`;
+
+export const Title = styled.p`
+    font-size: 1.4em;
+    font-weight: 700;
+`;
+
+export const Summary = styled.p`
+    margin-top: 0;
+    font-size: 1.1em;
 `;

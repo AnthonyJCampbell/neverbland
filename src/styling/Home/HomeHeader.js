55f333ee2f674fa00edf9e1423ffdcbaa3411b0f
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2020-03-12 11:53:58 +0100

    Styling for HomePage, except for Cards and CardContainer

--- src/styling/Home/HomeHeader.js
+++ src/styling/Home/HomeHeader.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
+import { styleConstants } from "../constants";
 
 export const StyledHomeHeader = styled.div`
-   
+    background: ${styleConstants.headerColor};
+    margin-top: -20px;
+    padding: 20px;
 `;
--- src/styling/Home/SelectCountry.js
+++ src/styling/Home/SelectCountry.js
@@ -1,5 +1,32 @@
 import styled from "styled-components";
+import { styleConstants } from "../constants";
 
 export const StyledSelectCountry = styled.div`
-   
+    margin-bottom: -20px; 
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    p {
+        padding: 0 4px;
+        border-bottom: 3px solid #aaa;
+        font-size: 1.4em;
+        max-width: 60%
+    }
+
+    div {
+        max-width: 30%;
+        select {
+            background: ${styleConstants.headerColor};
+            width: 100%;
+            border-bottom: 3px solid #aaa;
+            line-height: 1.3;
+            padding: .6em .6em .5em .8em;
+            border-radius: 8px;
+            &:hover { 
+                /* border: 1px solid #fff; */
+                border-bottom: 3px solid #ddd;
+            }
+        }
+    }
 `;
